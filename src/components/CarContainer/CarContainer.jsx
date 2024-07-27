dfa7320fde7bc1bf850d38e9c1a84f07216ee278
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2024-07-27 10:14:25 +0300

    added more props

--- src/components/CarContainer/CarContainer.jsx
+++ src/components/CarContainer/CarContainer.jsx
@@ -1,24 +1,38 @@
 import React from "react";
 import "./carcontainer.module.scss";
-import placeholder from "../../../images/placeholder.jpg";
-import { car, car__info, rent_button, icon } from "./carcontainer.module.scss";
+import {
+  car,
+  car__icons,
+  rent_button,
+  icon,
+  car__info,
+} from "./carcontainer.module.scss";
 import fuel_type_icon from "../../icons/fuel_type.svg";
 import year_icon from "../../icons/year.svg";
 import transmission_icon from "../../icons/transmision.svg";
+import info_icon from "../../icons/info.svg";
 
-function CarContainer({ name, year, fuel, transmission, price, image }) {
+function CarContainer({ make, model, year, fuel, transmission, price, image }) {
   return (
     <>
       <div className={car}>
         <img src={image} />
-        <h1>{name}</h1>
+        <h1>
+          {make} <span>{model}</span>
+        </h1>
         <p>{price}</p>
-        <div className={car__info}>
-          <img src={fuel_type_icon} className={icon}/> <span>{fuel} </span>
-          <img src={transmission_icon} className={icon}/> <span>{transmission}</span>
-          <img src={year_icon} className={icon}/> <span>{year}</span>
+        <div className={car__icons}>
+          <img src={fuel_type_icon} className={icon} /> <span>{fuel} </span>
+          <img src={transmission_icon} className={icon} />{" "}
+          <span>{transmission}</span>
+          <img src={year_icon} className={icon} /> <span>{year}</span>
         </div>
-        <button className={rent_button}>Rent Car</button>
+        <div className={car__info}>
+          <button className={rent_button}>Rent Car</button>
+          <button>
+            <img src={info_icon} alt="" />
+          </button>
+        </div>
       </div>
     </>
   );
