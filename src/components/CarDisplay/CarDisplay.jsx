ac680e57bfbfacf14167fba814e6e1bbc6efb263
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2024-10-28 23:01:47 +0200

    first commit

---
+++ src/components/CarDisplay/CarDisplay.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { car_display, buttons, images, fadeImage } from "./cardisplay.module.scss";
+import audi from "../../assets/cars/audi_r8.png";
+import passat from "../../assets/cars/passat.png";
+import toyota_yaris from "../../assets/cars/toyota_yaris.png"
+
+function CarDisplay() {
+  const carData = [
+    { name: "Audi R8", img: audi, price: "30$/h" },
+    { name: "Volkswagen Passat", img: passat, price: "15$/hr" },
+    { name: "Toyota Yaris", img: toyota_yaris, price: "50$/hr" },
+
+  ];
+
+  const [selectedCar, setSelectedCar] = useState(carData[0]);
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  const handleCarChange = (car) => {
+    setIsAnimating(true);
+    setTimeout(() => {
+      setSelectedCar(car);
+      setIsAnimating(false);
+    }, 500);  //time to match the transition time in cardisplay.module.scss
+  };
+
+  return (
+    <>
+      <h1>Take a look at some of our models</h1>
+      <br />
+      <div className={car_display}>
+        <div className={buttons}>
+          {carData.map((car, key) => (
+            <button key={key} onClick={() => handleCarChange(car)} data-back={car.price} data-front={car.name}>
+              {car.name}
+            </button>
+          ))}
+        </div>
+        <div className={images}>
+          <img
+            className={isAnimating ? fadeImage : ""}
+            src={selectedCar.img}
+            alt={selectedCar.name}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default CarDisplay;
