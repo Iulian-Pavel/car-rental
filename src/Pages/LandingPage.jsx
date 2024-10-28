cece8ec234c500b84e14aebd800ebf820bfb8e50
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2024-10-28 23:02:21 +0200

    Added Car Display component

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Header from "../components/Header/Header";
+import CarDisplay from "../components/CarDisplay/CarDisplay";
 
 function LandingPage() {
   return (
     <>
       <Navbar />
       <Header />
+      <CarDisplay />
     </>
   );
 }
