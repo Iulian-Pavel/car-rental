b6f4b53ca6f2dd4037b67ab4dbae68b67ede6c33
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2024-10-28 23:06:39 +0200

    Revert "Added Car Display component"
    This reverts commit cece8ec234c500b84e14aebd800ebf820bfb8e50.

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Header from "../components/Header/Header";
-import CarDisplay from "../components/CarDisplay/CarDisplay";
 
 function LandingPage() {
   return (
     <>
       <Navbar />
       <Header />
-      <CarDisplay />
     </>
   );
 }
