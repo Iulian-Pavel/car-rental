446e1168c9de79491e83050615389f35c5694d79
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2024-11-01 21:28:22 +0200

    created a separate hook for input control

---
+++ src/Hooks/useCarForm.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const useCarForm = () => {
+    const [carForm, setCarForm] = useState({
+      make: "",
+      model: "",
+      year: "",
+      fuelType: "",
+      transmissionType: "",
+      price: "",
+      linkToImage: ""
+    });
+
+    const handleInputChange = (e) => {
+        const {name, value} = e.target
+        setCarForm((prev) => ({...prev, [name]: value}))
+    } 
+
+    return { carForm, setCarForm, handleInputChange }
+  }
