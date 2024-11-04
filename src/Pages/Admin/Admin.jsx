e7b41c312a78537446f5a60b70fa6f79e570746b
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2024-11-04 23:18:33 +0200

    refactored code so now the car data is passed from the carForm object in useCarForm instead of the object being hardcoded into the try block

--- src/Pages/Admin/Admin.jsx
+++ src/Pages/Admin/Admin.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import InfoCard from "../../components/InfoCard/InfoCard";
-import axios from "axios";
+import Axios from "axios";
 import axiosInstance from "../../utils/interceptors";
+import useCarForm from "../../Hooks/useCarForm";
 import {
   dashboard,
   button,
@@ -11,6 +12,7 @@ import {
   modal_close,
 } from "./admin.module.scss";
 import placeholder from "../../../images/placeholder.jpg";
+
 const AddCarModal = ({ isOpen, onClose, onSubmit, carForm, onChange }) => (
   <div className={isOpen ? modal : modal_close}>
     <div className={modal_inputs}>
@@ -40,154 +42,58 @@ const RemoveCarModal = ({ isOpen, onClose, onSubmit, onChange }) => (
 );
 
 function Admin() {
-
-  const [isModalOpen, setIsModalOpen] = useState("false");
-  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState("false");
-  const [make, setMake] = useState("");
-  const [model, setModel] = useState("");
-  const [year, setYear] = useState("");
-  const [fuelType, setFuelType] = useState("");
-  const [transmissionType, setTransimissionType] = useState("");
-  const [price, setPrice] = useState("");
-  const [linkToImage, setLinkToImage] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const {carForm, handleInputChange, setCarForm} = useCarForm();
 
   const addCar = async () => {
-    let carData;
     try {
-      carData = {
-        make: make,
-        model: model,
-        year: year,
-        fuelType: fuelType,
-        transmissionType: transmissionType,
-        price: price,
-        linkToImage: linkToImage,
-      }
-      const response = await axiosInstance.post("/insert", carData);
-    } catch(error) {
-      console.log("An error occured while inserting car ", error);
-      console.log(response)
+      await axiosInstance.post("/insert", carForm);
+    } catch (error) {
+      console.error("Error adding car:", error);
     }
-  }
+  };
 
   const removeCar = async () => {
+    const { make, model, year } = carForm;
     try {
-      const response = axios.delete("http://localhost:3001/remove", {
-        data: {
-          make: make,
-          model: model,
-          year: year
-        }
+      const response = await Axios.delete("http://localhost:3001/remove", {
+        data: { make, model, year },
       });
-      console.log("Car deleted sucsessfully", response.data);
-    } catch(error) {
-      console.log("Error while removing car");
+      console.log("Car deleted successfully:", response.data);
+    } catch (error) {
+      console.error("Error removing car:", error);
     }
-  }
+  };
 
   return (
     <>
       <Navbar />
       <div className={dashboard}>
-        <InfoCard
-          title="Cars in fleet"
-          infoNumber={60}
-          image={placeholder}
-          bgcolor="blue"
-        />
-        <InfoCard
-          title="Cars available"
-          infoNumber={60}
-          image={placeholder}
-          bgcolor="blue"
-        />
-        <InfoCard
-          title="Total Revenue"
-          infoNumber={60}
-          image={placeholder}
-          bgcolor="blue"
-        />
+        <InfoCard title="Cars in fleet" infoNumber={60} image={placeholder} bgcolor="blue" />
+        <InfoCard title="Cars available" infoNumber={60} image={placeholder} bgcolor="blue" />
+        <InfoCard title="Total Revenue" infoNumber={60} image={placeholder} bgcolor="blue" />
       </div>
-      <button
-        onClick={() => setIsModalOpen(!isModalOpen)}
-        style={{ backgroundColor: "green" }}
-        className={button}
-      >
+      <button onClick={() => setIsModalOpen(true)} style={{ backgroundColor: "green" }} className={button}>
         Add car
       </button>
-      <button
-        style={{ backgroundColor: "red" }}
-        className={button}
-        onClick={() => setIsDeleteModalOpen(!isDeleteModalOpen)}
-      >
-        remove car
+      <button onClick={() => setIsDeleteModalOpen(true)} style={{ backgroundColor: "red" }} className={button}>
+        Remove car
       </button>
 
-      {/* Modal for adding the car */}
-      <div className={isModalOpen ? modal_close : modal}>
-        <div className={modal_inputs}>
-          <button onClick={() => setIsModalOpen(!isModalOpen)}>X</button>
-          <input
-            type="text"
-            placeholder="Make"
-            onChange={(e) => setMake(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Model"
-            onChange={(e) => setModel(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Year"
-            onChange={(e) => setYear(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Fuel Type"
-            onChange={(e) => setFuelType(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Transmission Type"
-            onChange={(e) => setTransimissionType(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Price"
-            onChange={(e) => setPrice(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Link to Image"
-            onChange={(e) => setLinkToImage(e.target.value)}
-          />
-          <button onClick={addCar}>add car</button>
-        </div>
-      </div>
-
-      {/* Remove car modal */}
-      <div className={isDeleteModalOpen ? modal_close : modal}>
-        <div className={modal_inputs}>
-          <button onClick={() => setIsDeleteModalOpen(!isDeleteModalOpen)}>X</button>
-          <input
-            type="text"
-            placeholder="Make"
-            onChange={(e) => setMake(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Model"
-            onChange={(e) => setModel(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Year"
-            onChange={(e) => setYear(e.target.value)}
-          />
-          <button onClick={removeCar}>remove car</button>
-        </div>
-      </div>
+      <AddCarModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onSubmit={addCar}
+        carForm={carForm}
+        onChange={handleInputChange}
+      />
+      <RemoveCarModal
+        isOpen={isDeleteModalOpen}
+        onClose={() => setIsDeleteModalOpen(false)}
+        onSubmit={removeCar}
+        onChange={handleInputChange}
+      />
     </>
   );
 }
