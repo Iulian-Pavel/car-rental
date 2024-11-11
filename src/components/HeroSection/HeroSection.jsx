d7135c76444b6b34d6374ee4c188d77c16cc75e0
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2024-11-12 00:17:31 +0200

    HeroSection completed

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,19 +1,55 @@
-import React from 'react';
-import {hero__container, hero__content, reasons} from "./herosection.module.scss";
-import placeholder from "../../../images/placeholder.jpg"
+import React from "react";
+import {
+  hero__container,
+  hero__content,
+  reasons,
+  reason,
+  hero__title,
+} from "./herosection.module.scss";
+import placeholder from "../../../images/placeholder.jpg";
 function HeroSection() {
   return (
     <>
-        <div className={hero__container}>
-            <div className={hero__content}>
-                <img src={placeholder} width={400} />
-                <div className={reasons}>
-                    <p>test</p>
-                </div>
+      <br />
+      <br />
+      <div className={hero__container}>
+        <h1 className={hero__title}>Why Choose Us?</h1>
+        <div className={hero__content}>
+          <img src={placeholder} width={400} />
+          <div className={reasons}>
+            <div className={reason}>
+              <h1>No Delays</h1>
+              <p>
+                This is a piece of informational text that contains a lot of
+                information about something on this website
+              </p>
             </div>
+            <div className={reason}>
+              <h1>No Delays</h1>
+              <p>
+                This is a piece of informational text that contains a lot of
+                information about something on this website
+              </p>
+            </div>
+            <div className={reason}>
+              <h1>No Delays</h1>
+              <p>
+                This is a piece of informational text that contains a lot of
+                information about something on this website
+              </p>
+            </div>
+            <div className={reason}>
+              <h1>No Delays</h1>
+              <p>
+                This is a piece of informational text that contains a lot of
+                information about something on this website
+              </p>
+            </div>
+          </div>
         </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
