a67671d065a3e3acc4d6c86161b3da6bcfe049ab
Iulian-Pavel <sirbuiulianpavel@gmail.com>
2024-11-18 23:24:53 +0200

    Added footer

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar/Navbar";
 import Header from "../components/Header/Header";
 import CarDisplay from "../components/CarDisplay/CarDisplay";
 import HeroSection from "../components/HeroSection/HeroSection";
+import Footer from "../components/Footer/Footer";
 
 function LandingPage() {
   return (
@@ -11,6 +12,7 @@ function LandingPage() {
       <Header />
       <CarDisplay />
       <HeroSection />
+      <Footer />
     </>
   );
 }
